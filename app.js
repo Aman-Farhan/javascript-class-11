aa8e04ddc169e1071539343af2ced843ce13d5d2
Aman <amanfarhan2008@gmail.com>
2025-07-21 23:47:48 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+// let text = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo accusantium a ut, doloribus magnam ex harum porro dignissimos commodi assumenda maiores. Officia ratione, est enim alias totam eaque consequatur nesciunt.";
+// console.log(text);
+// let makeUpper = true ;
+// let result = ""
+// for(let i = 0 ; i < text.length; i++){
+//     let 
+//     if(text[i] === " "){
+//         text[i].toUpperCase();
+//     }
+// }
+// console.log(text);
+
+
+
+// for(let i = 1 ; i > 8 ; i++){
+//    document.write("<h1>" + str +"</h1>")
+//     str = str.slice(0 , str.length - 1 );
+
+// }
+
+// let tableCount = +prompt("which table want to print");
+
+
+
+
+// let birthMonth = ["jan" , "feb", "mar","apr","may"];
+// let userBirthMonth = prompt("enter your birth month");
+
+// userBirthMonth = userBirthMonth.slice(0 , 3);
+// userBirthMonth = userBirthMonth.toLowerCase();
+
+// userBirthMonth = userBirthMonth.slice(9, 12);
+// userBirthMonth = userBirthMonth.toLowerCase();
+
+// console.log(userBirthMonth);
+
